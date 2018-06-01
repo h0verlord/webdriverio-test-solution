65a84d70811b69afca0639e49cfd9822d89eb083
rzevel <richard.zevel@gmail.com>
2018-06-01 13:54:40 +0200

    correcting errors eslint

--- sample-site/test/objects/cart.page.js
+++ sample-site/test/objects/cart.page.js
@@ -1,7 +1,13 @@
 class Cart {
-    get btn() { return $("#buyNowButton")}
-    get qty() { return $("#qty")}
-    get thankYou() { return $(".callout*=Thank you human")}
+  get btn() {
+    return $('#buyNowButton')
+  }
+  get qty() {
+    return $('#qty')
+  }
+  get thankYou() {
+    return $('.callout*=Thank you human')
+  }
 }
 
 module.exports = new Cart()
